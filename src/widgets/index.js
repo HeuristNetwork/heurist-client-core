44526a96aa52a6f2c86dc65c5529b460208793b5
Artem Osmakov <osmakov@gmail.com>
2026-09-13 11:27:37 +0400

    Remove widgets

--- src/widgets/index.js
+++ src/widgets/index.js
@@ -1,4 +1,1 @@
 export { HBaseWidget } from './HBaseWidget.js';
-export { HRecordList } from './HRecordList.js';
-export { HFilter, parseSavedFilterDefinition, normalizeDirectQuery, executableRequest } from './HFilter.js';
-export { HFilterForm } from './HFilterForm.js';
